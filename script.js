e3259f0b0f431c649577cf4791b99b0b51872927
Lucian Barbu <112772067+Luke-Brb@users.noreply.github.com>
2024-06-17 12:47:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const game = document.getElementById('game');
 const btnReset = document.getElementById('btnReset');
 const table = [[null, null, null],
-            [null, null, null],
-            [null, null, null]];
-
+               [null, null, null],
+               [null, null, null]];
 let l, c;
 let player = "X", moves = 0;
 btnReset.addEventListener('click', resetGame);
@@ -20,7 +19,7 @@ game.addEventListener('click', (e) => {
     if (gameOver(l, c, player)) {
         alert(`Congratulations ${player}! You win`);
         btnReset.disabled = false;
-    } else if(moves == 9) {
+    } else if (moves == 9) {
         alert('The game is a draw!');
         btnReset.disabled = false;
     } else {
@@ -28,46 +27,63 @@ game.addEventListener('click', (e) => {
     }
 });
 generateTable();
+
 function gameOver(l, c, player) {
     let cnt = 0;
-    for(let i = 0; i < 3; i++) { //verifica linia
-        if (table[l][i] == player)
+    //check the line
+    for (let i = 0; i < 3; i++) {
+        if (table[l][i] == player) {
             cnt++;
+        }
     }
-    if (cnt == 3) return true;
+    if (cnt == 3) {
+        return true;
+    }
     cnt = 0;
-    for(let i = 0; i < 3; i++) { //verifica coloana
-        if (table[i][c] == player)
+    //check the column
+    for (let i = 0; i < 3; i++) {
+        if (table[i][c] == player) {
             cnt++;
-    }
-    if (cnt == 3) return true;
-    
-    if (l == c) { // verifica diagonala principala
-        cnt = 0;
-        for (let i = 0; i < 3; i++) {
-            if (table[i][i] == player)
-                cnt++;
-        }
-    } else if (l + c == 2) { // verifica diagonala secundara
-        cnt = 0;
-        for (let i = 0; i < 3; i++) {
-            if (table[i][3 - i -1] == player)
-                cnt++;
         }
     }
-    if (cnt == 3) return true;
-    return false;
+    if (cnt == 3) {
+        return true;
+    }
+    // check the main diagonal
+    if (l == c) {
+        cnt = 0;
+        for (let i = 0; i < 3; i++) {
+            if (table[i][i] == player){
+                cnt++;
+            }
+        }
+    } else if (l + c == 2) { // check the secondary diagonal
+        cnt = 0;
+        for (let i = 0; i < 3; i++) {
+            if (table[i][3 - i - 1] == player){
+                cnt++;
+            }
+        }
+    }
+    if (cnt == 3) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
 function switchPlayer() {
-    if(player == "X")
+    if (player == "X") {
         player = "0";
-    else
-        player = "X"; // sau "player=player=="X" ? "0" : "X";
+    } else {
+        player = "X"; // or "player = player == "X" ? "0" : "X";
+    }
     document.getElementById('player').textContent = player;
 }
+
 function resetGame() {
-    for(let i = 0; i < 3; i++) {
-        for(let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
             table[i][j] = null;
         }
     }
@@ -78,12 +94,13 @@ function resetGame() {
     moves = 0;
     btnReset.disabled = true;
 }
+
 function generateTable() {
     let l, c;
     for (let i = 0; i < 9; i++) {
         let e = document.createElement('div');
-        l = Math.round((i+2)/3)-1;
-        c = Math.round(i%3);
+        l = Math.round((i + 2) / 3) -1;
+        c = Math.round(i % 3);
         e.setAttribute('l', l);
         e.setAttribute('c', c);
         game.appendChild(e);
